b9452ac37ff683543bb6d34f685d1837ee437d8f
Mikhail <skulllalka@gmail.com>
2025-09-12 14:11:37 +0300

    laptop 12.09.25 morning

--- main.go
+++ main.go
@@ -6,8 +6,10 @@ import (
 )
 
 func main() {
-	value1 := "rat"
-	value2 := "car"
+	// value1 := "rat"
+	// value2 := "car"
+	value := []int{0}
+	//fmt.Println(solution.IsAnagram(value1, value2))
 
-	fmt.Println(solution.IsAnagram(value1, value2))
+	fmt.Println(solution.MoveZeroes(value))
 }
--- solution/moveZero.go
+++ solution/moveZero.go
@@ -1,5 +1,17 @@
 package solution
 
-func MoveZeroes(nums []int) {
+func MoveZeroes(nums []int) []int {
+	left := 0
+
+	for right := 0; right < len(nums); right++ {
+		if nums[right] != 0 {
+			temp := nums[right]
+			nums[right] = nums[left]
+			nums[left] = temp
+
+			left++
+		}
+	}
+	return nums
 
 }
